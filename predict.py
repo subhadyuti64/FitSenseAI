414ae52b2d171c9bc809a61d6f03a13a763dd1e6
subhadyuti64 <subhadyutirath@gmail.com>
2025-07-20 17:11:19 +0530

    Predict app added

--- predict.py
+++ predict.py
@@ -34,7 +34,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     if st.button("Classify"):
         with st.spinner('Loading model and predicting...'):
             model = load_model()
